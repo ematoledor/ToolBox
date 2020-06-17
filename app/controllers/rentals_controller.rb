f415b09caed7eefb0b37d22599bd20e1b38d7c03
Emanuel Toledo <ematolr@gmail.com>
2020-06-17 14:52:30 -0500

    added view for dates and action

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,28 @@
 class RentalsController < ApplicationController
 
+    def new
+        @rental = Rental.new
+        @tool = Tool.find(params[:tool_id])
+    end
+
     def create
-        
+        @tool = Tool.find(params[:tool_id])
+        @rental = Rental.new(rental_params)
+        @rental.tool = @tool
+        @rental.user = current_user
+        @rental.save
+
+        redirect_to tool_path(@tool)    
+    end
+
+    def destroy
+        @rental = Rental.find(params[:id])
+        @rental.destroy
+        redirect_to tool_path(@rental.tool)
+    end
+
+    def rental_params
+        params.require(:rental).permit(:start_date, :end_date)
     end
 
 end
