23f7e131e9f6477688417406ad3b508304fa9b8c
Bhanu Gummadi <prakash@codejek.com>
2020-06-17 11:23:03 -0700

    Seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+puts "Destroying the existing Users and Tools !!!"
+User.destroy_all
+Tool.destroy_all
+puts "Creating the User -"
+user_1 = User.create!(email: 'user@test.com', password: 'welcme1')
+condition = ['Brand New', 'Moderately Used', 'Warranty Expired', 'Recently Serviced']
+
+puts 'Creating 50 tools..'
+50.times do |i|
+  tool = Tool.create!(
+    name:    Faker::Appliance.equipment,
+    description: Faker::Appliance.brand,
+    condition: condition.sample,
+    price: Faker::Commerce.price(range: 1..10.0, as_string: true),
+    user: user_1
+  )
+  puts "#{i + 1}.  #{tool.name}, --> #{tool.condition},     --> $#{tool.price} USD"
+end
+puts 'Finished!'
