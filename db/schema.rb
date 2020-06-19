0c21274c2404cae882505f4162efd24ab8360623
Emanuel Toledo <ematolr@gmail.com>
2020-06-19 13:24:25 -0500

    moved map and products into containers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_18_171955) do
+ActiveRecord::Schema.define(version: 2020_06_18_185257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
